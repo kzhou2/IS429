4a1c801c6a9e47cd47a079f1a57438161c92112c
kzhou2 <kzhou2@e.ntu.edu.sg>
2014-04-06 18:48:25 +0800

    wewefwe

--- V.13/mastermind.js
+++ V.13/mastermind.js
@@ -17,7 +17,7 @@ colorImages  = ['redball.gif',    'blueball.gif',    'brownball.gif',
                 'greenball.gif',  'yellowball.gif',  'pinkball.gif' ];
 
 // --------------------------------------------------------
-// called when user clicks "New Game" - git 192234.
+// called when user clicks "New Game" - git kk192234.
 function newGame(){
     resetGame();
     gameNumber++;
